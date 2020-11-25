d1f3feb49a83f17ef4635d7d2582ab4349604ecb
Evan Pratten <ewpratten@gmail.com>
2020-11-25 13:09:24 -0500

    Update CI scripts

--- tools/scripts/ci/jitpack_setup.sh
+++ tools/scripts/ci/jitpack_setup.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# Install GCC
+sudo apt-get update -y
+sudo apt-get install build-essential -y
+
+# Install bazel
 curl -LO "https://github.com/bazelbuild/bazelisk/releases/download/v1.7.4/bazelisk-linux-amd64"
 mkdir -p ./bin
 mv bazelisk-linux-amd64 ./bin/bazel
